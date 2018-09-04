31a7c46029f76e5ca5d3d0232502b38832880241
Jaci Brunning <jaci.brunning@gmail.com>
2018-09-05 00:01:40 +0800

    Add benchmarks for trapezoidal and hermites

---
+++ src/bench/path/bench_arc_param.cpp
@@ -0,0 +1,37 @@
+#include "grpl/path/arc_parameterizer.h"
+#include "grpl/path/hermite.h"
+
+#include <vector>
+
+using namespace grpl;
+using namespace grpl::path;
+
+#include <benchmark/benchmark.h>
+
+static void BM_ArcParamHermite(benchmark::State &state) {
+  using hermite_t = hermite_quintic;
+
+  hermite_t::waypoint start{{2, 2}, {5, 0}, {0, 0}}, end{{5, 5}, {5, 5}, {0, 0}};
+  hermite_t           hermite(start, end);
+
+  int num_curves = 0, num_iter = 0;
+
+  for (auto _ : state) {
+    state.PauseTiming();
+    std::vector<arc_parameterizer::curve_t> curves;
+    arc_parameterizer param;
+    double sensitivity = 1.0 / static_cast<double>(state.range(0));
+    param.configure(sensitivity, sensitivity);
+    curves.reserve(param.curve_count(hermite));   // Ensure the timing isn't affected by reallocs
+    state.ResumeTiming();
+
+    param.parameterize(hermite, std::back_inserter(curves), curves.max_size());
+    num_curves += curves.size();
+    num_iter++;
+  }
+
+  state.counters["NumCurves"] = num_curves / num_iter;
+  state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(BM_ArcParamHermite)->Arg(1)->Arg(10)->Arg(100)->Arg(1000)->Complexity();
---
+++ src/bench/profile/bench_trapezoidal.cpp
@@ -0,0 +1,25 @@
+#include <grpl/profile/trapezoidal.h>
+
+#include <benchmark/benchmark.h>
+
+using namespace grpl::profile;
+
+static void BM_Profile_Trapezoidal(benchmark::State &state) {
+  trapezoidal pr;
+  pr.apply_limit(1, -3, 3);   // Velocity Limit = -3 to 3m/s
+  pr.apply_limit(2, -3, 4);   // Acceleration limit = -3 to 4m/s
+  pr.set_goal(5);             // Goal = 5m
+  pr.set_timeslice(0);        // No Timeslice
+
+  for (auto _ : state) {
+    trapezoidal::segment_t seg;
+    double dt = 1.0 / static_cast<double>(state.range(0));
+    for (double t = 0; t < 10; t += dt) {
+      benchmark::DoNotOptimize(seg = pr.calculate(seg, t));
+    }
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(BM_Profile_Trapezoidal)->Arg(10)->Arg(100)->Arg(1000)->Arg(10000)->Complexity();
